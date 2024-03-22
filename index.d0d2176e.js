8f3d7377d47243919e0542004e773e680c504f0c
Romanooo84 <133641494+Romanooo84@users.noreply.github.com>
2024-03-22 23:43:12 +0000

    Deploying to gh-pages from @ Romanooo84/stock-data@72ca952190ff03842e0a00cce9404c454cbd8615 🚀

--- index.d0d2176e.js
+++
@@ -1,2 +0,0 @@
-fetch("https://eodhd.com/api/real-time/AAL.war?api_token=65fe0a1382dc25.48851936&fmt=json").then((function(o){if(!o.ok)throw new Error("Network response was not ok");return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.error("There was a problem with your fetch operation:",o)}));
-//# sourceMappingURL=index.d0d2176e.js.map
