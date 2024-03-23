e0861dc90ec08fb9b8129f3b85835f6d930737dc
Romanooo84 <133641494+Romanooo84@users.noreply.github.com>
2024-03-23 07:49:09 +0000

    Deploying to gh-pages from @ Romanooo84/stock-data@ac545a0975d789c09444f3ba183950aac67bea33 🚀

--- index.398a9a1a.js
+++
@@ -1,2 +0,0 @@
-fetch("https://eodhd.com/api/real-time/ETFBM40TR.war?api_token=65fe0a1382dc25.48851936&fmt=json").then((o=>{if(!o.ok)throw new Error("Network response was not ok");return o.json()})).then((o=>{console.log(o)})).catch((o=>{console.error("There was a problem with your fetch operation:",o)}));
-//# sourceMappingURL=index.398a9a1a.js.map
