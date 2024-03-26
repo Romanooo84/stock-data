aed05f1b90f7a287da268e04dc1199ce26c6709b
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-03-26 16:47:33 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { lineChart, dataGraph } from "./js/graph.js";
+import { lineChart, dataGraph, updateChart, removeData } from "./js/graph.js";
 import { historicalStockData, dailyStockData, exchangeList, interdayData } from "./js/import_data.js";
 import { particularData } from "./js/particular_data.js";
 
 const token = '65fd2d716aebf2.80647901';
 const exchange = 'WAR';
-const ticker = 'ETFBM40TR';
+const ticker = 'ETFBW20LV';
 const index = ticker.concat('.', exchange)
+ctx = document.getElementById('myChart');
 let chartData
 
 // pobranie danych o giełdach
@@ -44,11 +45,15 @@ let button = document.querySelector('.button')
 console.log(button)
 button.addEventListener('click', function () { console.log(chartData) })
   
-const timerId = setInterval(() => {
+const timerId = setInterval(() => { 
   dailyStockData(index, token)
   .then(dailyData => {
-        dataGraph(dailyData, chartData)
-        particularData('currentData', ticker, dailyData.change_p)
-    console.log('updated')
-      })
-}, 50000);
+    dataGraph(dailyData, chartData)
+    particularData('currentData', ticker, dailyData.change_p)
+  })
+  newDataChart.options.animation = false;
+  newDataChart.update()
+
+}, 10000);
+
+  
--- src/js/graph.js
+++ src/js/graph.js
@@ -2,43 +2,61 @@
 export function lineChart(xAxis, yAxis, ticker) {
   const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
+  newDataChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: xAxis,
       datasets: [{
         label: ticker,
         data: yAxis,
-        borderWidth: 1
+        borderWidth: 1,
+        pointRadius: 1,
       }]
     },
     options: {
+
     }
   });
+  return newDataChart
+}
 
+//dodawanie danych do wykresu
+
+export function addData(chart, label, newData) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(newData);
+  });
+  chart.update();
+}
+  //usuwanie danych z wykresu
+export function updateChart(chart,newXAxis, newYAxis) {
+  chart.data.labels = newYAxis
+  chart.data.datasets[0].data=newXAxis
+  chart.update();
 }
 
 // pobranie danych do wykresu
 
 export function dataGraph(dailyData, historicalData) {
   //przetworzenie bieżacej daty z unix na normalną
-        let currentDateUnix = dailyData.timestamp // pobranie daty
-        let currentDate = new Date(currentDateUnix * 1000) //przerobienie daty
-        // wybranie roku miesiąca i dnia
-        const year = currentDate.getFullYear();
-        const month = ('0' + (currentDate.getMonth() + 1)).slice(-2); // Dodanie 1, bo getMonth() zwraca miesiące od 0 do 11, oraz formatowanie do dwóch cyfr
-        const day = ('0' + currentDate.getDate()).slice(-2); // Formatowanie do dwóch cyfr
-        currentDate = `${year}-${month}-${day}`;
-        // pobranie ostatniej daty z osi czasu
-        let lastAxisDate = historicalData.xAxis[historicalData.xAxis.length - 1]
-        // sprawdzenie dat
-        if (currentDate > lastAxisDate) {
-          historicalData.xAxis.push(currentDate)
-          historicalData.yAxis.push(dailyData.close)
-        }
-        else if(currentDate === lastAxisDate) {
-          historicalData.yAxis.pop()
-          historicalData.yAxis.push(dailyData.close)
-        }
+  let currentDateUnix = dailyData.timestamp // pobranie daty
+  let currentDate = new Date(currentDateUnix * 1000) //przerobienie daty
+  // wybranie roku miesiąca i dnia
+  const year = currentDate.getFullYear();
+  const month = ('0' + (currentDate.getMonth() + 1)).slice(-2); // Dodanie 1, bo getMonth() zwraca miesiące od 0 do 11, oraz formatowanie do dwóch cyfr
+  const day = ('0' + currentDate.getDate()).slice(-2); // Formatowanie do dwóch cyfr
+  currentDate = `${year}-${month}-${day}`;
+  // pobranie ostatniej daty z osi czasu
+  let lastAxisDate = historicalData.xAxis[historicalData.xAxis.length - 1]
+  // sprawdzenie dat
+  if (currentDate > lastAxisDate) {
+    historicalData.xAxis.push(currentDate)
+    historicalData.yAxis.push(dailyData.close)
+  }
+  else if (currentDate === lastAxisDate) {
+    historicalData.yAxis.pop()
+    console.log(dailyData.close)
+    historicalData.yAxis.push(dailyData.close)
+  }
 }
-
