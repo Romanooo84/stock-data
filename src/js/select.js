a6b52d0ba82d32dfb8825c384d5fb9373a7d28ea
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-03-27 12:49:54 +0100

    Update select.js

--- src/js/select.js
+++ src/js/select.js
@@ -3,7 +3,7 @@ import { exchangeListJson } from "./exchange_list.js";
 
 console.log(exchangeListJson[0].Code)
 
-let selectLabels = []
+let selectLabels = [{'placeholder': true, 'text': 'Select the stock exchange'}]
 
 for (let i = 0; i < exchangeListJson.length; i++){
   console.log(`https://eodhd.com/api/exchange-symbol-list/${exchangeListJson[i].Code}?api_token=65fd2d716aebf2.80647901&fmt=csv`)
@@ -16,7 +16,6 @@ for (let i = 0; i < exchangeListJson.length; i++){
 
 new SlimSelect({
   select: '#select',
-  {'placeholder': true, 'text': 'Select the stock exchange'}
   data: selectLabels
   
       })
