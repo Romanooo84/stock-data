881e468541afc5a5640400570c8ad799335d9e06
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-03-27 22:28:05 +0100

    select

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { lineChart, dataGraph, updateChart, removeData } from "./js/graph.js";
 import { historicalStockData, dailyStockData, exchangeList, interdayData } from "./js/import_data.js";
 import { particularData } from "./js/particular_data.js";
 import { exchangeListJson } from "./js/exchange_list.js";
+import { selectEx, select2 } from "./js/select.js";
 import './js/select.js'
 
 const token = '65fd2d716aebf2.80647901';
@@ -60,7 +61,27 @@ const timerId = setInterval(() => {
   newDataChart.options.animation = false;
   newDataChart.update()
 
-}, 10000);
+}, 60000);
+
+selectEx.addEventListener('change', function (event) {
+  event.preventDefault()
+  let selectedEx=selectEx.options[selectEx.selectedIndex].value
+  for (let i = 0; i < exchangeListJson.length;i++)
+    if (exchangeListJson[i].Name === selectedEx) {
+      select2Options = []
+      select2.setData(select2Options)
+      exchangeCode = exchangeListJson[i].Code
+      fetch(`https://eodhd.com/api/exchange-symbol-list/${exchangeCode}?api_token=65fd2d716aebf2.80647901&fmt=json`)
+        .then(data => data.json())
+        .then(data => {
+          for (let i = 0; i < data.length; i++) {
+            select2Options.push({ text: `${ data[i].Name }`})
+          }
+          return(select2Options)
+        })
+        .then(select2Options=>select2.setData(select2Options))
+  }
+})
 
 
 
--- src/js/select.js
+++ src/js/select.js
@@ -1,6 +1,5 @@
-
 import { exchangeListJson } from "./exchange_list.js";
-let selectEx = document.getElementById('select1');
+export let selectEx = document.getElementById('select1');
 let select1Options = [{ 'placeholder': true, 'text': 'Select the stock exchange' }]
 let select2Options = []
 
@@ -13,29 +12,11 @@ let select1=new SlimSelect({
   data: select1Options
 })
 
-let select2=new SlimSelect({
+export let select2=new SlimSelect({
   select: '#select2',
   data: select2Options
 })
 
-selectEx.addEventListener('change', function (event) {
-  event.preventDefault()
-  let selectedEx=selectEx.options[selectEx.selectedIndex].value
-  for (let i = 0; i < exchangeListJson.length;i++)
-    if (exchangeListJson[i].Name === selectedEx) {
-      select2Options = []
-      select2.setData(select2Options)
-      exchangeCode = exchangeListJson[i].Code
-      fetch(`https://eodhd.com/api/exchange-symbol-list/${exchangeCode}?api_token=65fd2d716aebf2.80647901&fmt=json`)
-        .then(data => data.json())
-        .then(data => {
-          for (let i = 0; i < data.length; i++) {
-            select2Options.push({ text: `${ data[i].Name }`})
-          }
-          return(select2Options)
-        })
-        .then(select2Options=>select2.setData(select2Options))
-  }
-})
+
 
       
