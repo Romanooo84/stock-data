fb95458959d7b4b41bfaa9cffd613724d7cd43f7
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-03-28 08:22:04 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { lineChart, dataGraph, updateChart, removeData } from "./js/graph.js";
 import { historicalStockData, dailyStockData, exchangeList, interdayData } from "./js/import_data.js";
 import { particularData } from "./js/particular_data.js";
 import { exchangeListJson } from "./js/exchange_list.js";
-import { selectEx, select2, select2Options } from "./js/select.js";
+import { selectEx, select2 } from "./js/select.js";
 import './js/select.js'
 
 const token = '65fd2d716aebf2.80647901';
@@ -68,7 +68,7 @@ selectEx.addEventListener('change', function (event) {
   let selectedEx=selectEx.options[selectEx.selectedIndex].value
   for (let i = 0; i < exchangeListJson.length;i++)
     if (exchangeListJson[i].Name === selectedEx) {
-      select2Options = []
+      let select2Options = []
       select2.setData(select2Options)
       exchangeCode = exchangeListJson[i].Code
       fetch(`https://eodhd.com/api/exchange-symbol-list/${exchangeCode}?api_token=65fd2d716aebf2.80647901&fmt=json`)
