5f6fca4c8375a38a1c218ea0c3d3eb146cb00ff1
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-03-29 11:58:34 +0100

    Update particular_data.js

--- src/js/particular_data.js
+++ src/js/particular_data.js
@@ -2,5 +2,6 @@ export function particularData(id, stockIndex, currentlyData) {
     let container = document.getElementById(`${id}`);
     container.innerHTML=''
     let map = `<p>${stockIndex} <span></span>${currentlyData}%</p>`
+    console.log(currentlyData)
     container.insertAdjacentHTML('beforeEnd', map)
 }
