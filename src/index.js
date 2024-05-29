7793ad174e29b3803938f8019aaa18b893952fc8
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-05-29 10:25:10 +0000

    delete updatre

--- src/index.js
+++ src/index.js
@@ -47,13 +47,18 @@ multipleDailyData(headerTickersList, token)
     } else if (ticker.change_p < 0) {
         changeColor = 'value-minus';
     }
-    return `<div>
+    return `<div class='short-data-div'>
               <div class='single-data' name = '${ticker.code}'>
                 <p class='header-paragraph'>${ticker.code}</p>
                 <p class='header-paragraph'>Close: ${ticker.close}</p>
                 <p class='header-paragraph'>Change: <span class='${changeColor}'>${ticker.change_p}%</span></p>
               </div>
-              <button type="button" name="${ticker.code}">X</button>
+              <button class = 'delete-button'  type="button" id="${ticker.code}" >
+                <svg id = '${ticker.code}' version="1.1" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32" fill = '#ec7c7c'>
+                  <path name = '${ticker.code}' d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+                  <path name = '${ticker.code}' d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
+                </svg>
+              </button>
             </div>`;
   }).join("");
       
@@ -81,14 +86,19 @@ multipleDailyData(headerTickersList, token)
     } else if (ticker.change_p < 0) {
         changeColor = 'value-minus';
       }
-    return `<div>
-    <div class='single-data' name = '${ticker.code}'>
-      <p class='header-paragraph'>${ticker.code}</p>
-      <p class='header-paragraph'>Close: ${ticker.close}</p>
-      <p class='header-paragraph'>Change: <span class='${changeColor}'>${ticker.change_p}%</span></p>
-    </div>
-    <button type="button" name="${ticker.code}">X</button>
-  </div>`;
+      return `<div class='short-data-div'>
+            <div class='single-data' name = '${ticker.code}'>
+              <p class='header-paragraph'>${ticker.code}</p>
+              <p class='header-paragraph'>Close: ${ticker.close}</p>
+              <p class='header-paragraph'>Change: <span class='${changeColor}'>${ticker.change_p}%</span></p>
+            </div>
+            <button class = 'delete-button'  type="button" id="${ticker.code}" >
+              <svg id = '${ticker.code}' version="1.1" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32" fill = '#ec7c7c'>
+                <path name = '${ticker.code}' d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+                <path name = '${ticker.code}' d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
+              </svg>
+            </button>
+          </div>`;
   }).join("");
       
       // Wstawianie wygenerowanego kodu HTML do elementu headerData
@@ -97,12 +107,14 @@ multipleDailyData(headerTickersList, token)
   .catch(error => {
     console.error("Wystąpił błąd podczas pobierania danych:", error);
   });
-},2000
+},200000
 );
 
 headerData.addEventListener('click',function (event){
   event.preventDefault()
-  let tickerToDelete=event.target.name
+  console.log(event.target.parentNode.id)
+  console.log(event.target.parentNode)
+  let tickerToDelete=event.target.parentNode.id
   headerTickersList = headerTickersList .filter(ticker => ticker !== tickerToDelete);
   multipleDailyData(headerTickersList, token)
   .then(data => {
@@ -114,13 +126,18 @@ headerData.addEventListener('click',function (event){
     } else if (ticker.change_p < 0) {
         changeColor = 'value-minus';
     }
-    return `<div>
+    return `<div class='short-data-div'>
               <div class='single-data' name = '${ticker.code}'>
                 <p class='header-paragraph'>${ticker.code}</p>
                 <p class='header-paragraph'>Close: ${ticker.close}</p>
                 <p class='header-paragraph'>Change: <span class='${changeColor}'>${ticker.change_p}%</span></p>
               </div>
-              <button type="button" name="${ticker.code}">X</button>
+              <button class = 'delete-button'  type="button" id="${ticker.code}" >
+                <svg id = '${ticker.code}' version="1.1" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32" fill = '#ec7c7c'>
+                  <path name = '${ticker.code}' d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+                  <path name = '${ticker.code}' d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
+                </svg>
+              </button>
             </div>`;
   }).join("");
       
