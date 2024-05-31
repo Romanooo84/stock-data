b46e602b07c0ae2ce0d106789eefaf6a2aefa7e4
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-05-31 12:02:07 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ multipleDailyData(headerTickersList, token)
       
       // Wstawianie wygenerowanego kodu HTML do elementu headerData
       headerData.insertAdjacentHTML("beforeend", markup);
-      delButton = document.getElementsByClassName('.delete-button')
+      delButton = document.querySelectorAll('.delete-button')
       console.log(delButton)
   })
   .catch(error => {
@@ -125,49 +125,53 @@ multipleDailyData(headerTickersList, token)
 },2000000000
 );
 
-  headerData.addEventListener('click',function (event){
-  event.preventDefault()
-  let tickerToDelete=event.target.parentNode.id
-  headerTickersList = headerTickersList .filter(ticker => ticker !== tickerToDelete);
-  multipleDailyData(headerTickersList, token)
-  .then(data => {
-    let newData = data
-    const markup = newData.map((ticker) => {
-    let changeColor = ''; // Initialize changeColor variable
-    if (ticker.change_p > 0) {
-        changeColor = 'value-plus';
-    } else if (ticker.change_p < 0) {
-        changeColor = 'value-minus';
-    }
-    return `<div class='short-data-div'>
-                <button class = 'delete-button'  type="button" id="${ticker.code}" >
-                <svg id = '${ticker.code}' version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32" fill = '#ec7c7c'>
-                  <path name = '${ticker.code}' d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
-                  <path name = '${ticker.code}' d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
-                </svg>
-              </button>
-              <div class='single-data' name = '${ticker.code}'>
-                <p class='header-paragraph'>${ticker.code}</p>
-                <p class='header-paragraph'>Close: ${ticker.close}</p>
-                <p class='header-paragraph'>Change: <span class='${changeColor}'>${ticker.change_p}%</span></p>
-              </div>
-             
-            </div>`;
-    }).join("");
-      
-      // Wstawianie wygenerowanego kodu HTML do elementu headerData
-      headerData.innerHTML = ''
-      headerData.insertAdjacentHTML("beforeend", markup);
-      delButton = document.getElementsByClassName('.delete-button')
-      console.log(delButton)
-      for (var i = 0; i < delButton.length; i++) {
-        delButton[i].disabled = true;
-    }
-    })
+headerData.addEventListener('click',function (event){
+    event.preventDefault()
+    let tickerToDelete=event.target.parentNode.id
+    headerTickersList = headerTickersList .filter(ticker => ticker !== tickerToDelete);
+    multipleDailyData(headerTickersList, token)
+    .then(data => {
+      let newData = data
+      const markup = newData.map((ticker) => {
+      let changeColor = ''; // Initialize changeColor variable
+      if (ticker.change_p > 0) {
+          changeColor = 'value-plus';
+      } else if (ticker.change_p < 0) {
+          changeColor = 'value-minus';
+      }
+      return `<div class='short-data-div'>
+                  <button class = 'delete-button'  type="button" id="${ticker.code}" >
+                  <svg id = '${ticker.code}' version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32" fill = '#ec7c7c'>
+                    <path name = '${ticker.code}' d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+                    <path name = '${ticker.code}' d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
+                  </svg>
+                </button>
+                <div class='single-data' name = '${ticker.code}'>
+                  <p class='header-paragraph'>${ticker.code}</p>
+                  <p class='header-paragraph'>Close: ${ticker.close}</p>
+                  <p class='header-paragraph'>Change: <span class='${changeColor}'>${ticker.change_p}%</span></p>
+                </div>
+              
+              </div>`;
+      }).join("");
+        
+        // Wstawianie wygenerowanego kodu HTML do elementu headerData
+        headerData.innerHTML = ''
+        headerData.insertAdjacentHTML("beforeend", markup);
+        delButton = document.querySelectorAll('.delete-button')
+        console.log(delButton)
+        if (delButton.length<3){
+        for (var i = 0; i < delButton.length; i++) {
+          delButton[i].disabled = true;
+      }}
+      if (headerTickersList.length <5){
+        favButton.disabled=false
+      }
+      })
 
-  .catch(error => {
-    console.error("Wystąpił błąd podczas pobierania danych:", error);
-  });
+    .catch(error => {
+      console.error("Wystąpił błąd podczas pobierania danych:", error);
+    });
 })
 
 
